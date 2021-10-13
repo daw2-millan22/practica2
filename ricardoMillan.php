d10c998b396f3b282e179d35b4da151e26c9561c
daw2-millan22 <91274985+daw2-millan22@users.noreply.github.com>
2021-10-13 23:06:03 +0200

    Add files via upload

---
+++ ricardoMillan.php
@@ -0,0 +1,8 @@
+</head>
+<body>
+	<?php 
+	echo "lo mejor que he aprendido en este documento es:<br>-aprender a verificar datos<br>-pasar datos de un link a otro<br>-'la funcion SESSION' <br>-'la funcion GET'<br>-'la funcion POST' <br>-'la funcion COOKIE'<br>-Aprendí a verificar variables nulas y darles valores por defecto si es que son nulas<br>-Aprendi funciones que nos dicen la fecha<br>-aprendi a usar un poco mas github<br>-aprendi que se pueden extraer algunos datos por la url";
+?>
+</body>
+
+
